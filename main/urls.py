f9a65c45e908d2bb7e7c2ce445630dce45954078
xamfy <spratyaksh22@gmail.com>
2019-05-06 23:50:15 +0530

    Basic app

---
+++ main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from main import views
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name="home.html"), name="home"),
+]
