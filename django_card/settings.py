c2404c5f68d8b5bd2fe1c6247ebd8879d840b730
xamfy <spratyaksh22@gmail.com>
2019-05-09 18:43:32 +0530

    add design

--- django_card/settings.py
+++ django_card/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'accounts',
-    'colorfield'
+    'colorfield',
+    'django_social_share',
 ]
 
 MIDDLEWARE = [
--- main/views.py
+++ main/views.py
@@ -78,35 +78,3 @@ def edit(request, pic):
 def generate(request, pic):
     pic = '/media/generated/'+pic+'.jpg'
     return render(request, "generated.html", {'src': pic})
-
-    # print(name)
-    # print(temp.cover)
-
-    # create Image object with the input image
-    # image = Image.open('media/'+str(temp.cover))
-
-    # initialise the drawing context with
-    # the image object as background
-
-    # draw = ImageDraw.Draw(image)
-
-    # font = ImageFont.truetype('main/static/fonts/Roboto-Bold.ttf', size=45)
-
-    # starting position of the message
-
-    # (x, y) = (temp.x, temp.y)
-    # message = "Happy Birthday!"
-    # color = 'rgb(0, 0, 0)'  # black color
-
-    # draw the message on the background
-
-    # draw.text((x, y), message, fill=color, font=font)
-    # image.save('greeting_card.png')
-
-    # (x, y) = (150, 150)
-    # name = 'asdfsdfsd'
-    # color = 'rgb(255, 255, 255)'  # white color
-    # draw.text((x, y), name, fill=color, font=font)
-    # # image.show()
-
-    # save the edited image
